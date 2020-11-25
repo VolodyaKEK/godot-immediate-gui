8e06ec0f92be9ad0efa44166f7355508cc9bf270
VolodyaKEK <63250343+VolodyaKEK@users.noreply.github.com>
2020-11-25 09:05:10 +0500

    Update example.gd

--- addons/godot-immediate-gui/example.gd
+++ addons/godot-immediate-gui/example.gd
@@ -5,6 +5,17 @@ var text = "text";
 var color = Color.white;
 var integer := 0;
 var float_value := 0.2;
+var option = 0;
+
+func _ready():
+	#By default all Controls have no size_flags
+	#and all Buttons have action_mode set to ACTION_MODE_BUTTON_PRESS.
+	#You can clear all default values by calling GUI.clear_default();
+	#And you can set your own default values.
+	#Call this to make all buttons flat by default
+	GUI.add_default(Button, "flat", true);
+	#Call this to remove default value "flat" from all Buttons
+	GUI.remove_default(Button, "flat");
 
 func _gui(delta):
 	#Label
@@ -39,11 +50,14 @@ func _gui(delta):
 	integer = GUI.spin(integer, 0, 20);
 	float_value = GUI.spin(float_value, 0.0, 1.0);
 	
+	#Options
+	option = GUI.options(option, ["option 1", "option 2", "option 3"]);
+	
 	#Progress bar
 	GUI.property.rect_min_size = Vector2(200, 0);
 	GUI.progress(float_value);
 	
-	#Utility
+	#Tooltip
 	GUI.property.rect_min_size = Vector2(200, 0);
 	GUI.property.hint_tooltip = "Tooltip";
 	GUI.button("Button with tooltip");
